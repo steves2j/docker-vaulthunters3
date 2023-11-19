d1dc5b247be05cc1f931a263de2b3a33fada56a7
Jason McLeod <jason@mcleods.me>
2023-11-19 08:06:55 -0500

    Update to version 3.11.2

--- launch.sh
+++ launch.sh
@@ -22,7 +22,7 @@ fi
 
 if ! [[ -f "Vault-Hunters-3rd-Edition-3.11.2-server-files.zip" ]]; then
   rm -fr config mods defaultconfigs scripts forge-*.jar start.sh *Server.zip
-	curl -Lo 'Vault-Hunters-3rd-Edition-3.11.2-server-files.zip' 'https://edge.forgecdn.net/files/4687/199/Vault-Hunters-3rd-Edition-3.11.2-server-files.zip' && unzip -u -o 'Vault-Hunters-3rd-Edition-3.11.2-server-files.zip' -d /data
+	curl -Lo 'Vault-Hunters-3rd-Edition-3.11.2-server-files.zip' 'https://edge.forgecdn.net/files/4685/339/Vault-Hunters-3rd-Edition-3.11.2-server-files.zip' && unzip -u -o 'Vault-Hunters-3rd-Edition-3.11.2-server-files.zip' -d /data
   curl -Lo forge-${FORGE_VERSION}-installer.jar 'https://maven.minecraftforge.net/net/minecraftforge/forge/'${FORGE_VERSION}'/forge-'${FORGE_VERSION}'-installer.jar'
   java -jar forge-${FORGE_VERSION}-installer.jar --installServer && rm -f forge-${FORGE_VERSION}-installer.jar
 fi
