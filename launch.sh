62790f1a615916fdd4cfe10a21c934d6cb1eba73
Jason McLeod <jason@mcleods.me>
2023-11-19 09:55:19 -0500

    Update to version 3.12.4

--- launch.sh
+++ launch.sh
@@ -7,7 +7,7 @@ if ! [[ -w "/data" ]]; then
   exit 66
 fi
 
-FORGE_VERSION=1.18.2-40.1.61
+FORGE_VERSION=1.18.2-40.2.9
 # https://maven.minecraftforge.net/net/minecraftforge/forge/1.18.2-40.1.61/forge-1.18.2-40.1.61-installer.jar
 
 cd /data
@@ -20,9 +20,9 @@ else
 fi
 
 
-if ! [[ -f "Vault Hunters 3rd Edition - 0.0.1 - Server.zip" ]]; then
+if ! [[ -f "Vault-Hunters-3rd-Edition-3.12.4-server-files.zip" ]]; then
   rm -fr config mods defaultconfigs scripts forge-*.jar start.sh *Server.zip
-	curl -Lo 'Vault Hunters 3rd Edition - 0.0.1 - Server.zip' 'https://edge.forgecdn.net/files/4119/443/Vault Hunters 3rd Edition - 0.0.1 - Server.zip' && unzip -u -o 'Vault Hunters 3rd Edition - 0.0.1 - Server.zip' -d /data
+	curl -Lo 'Vault-Hunters-3rd-Edition-3.12.4-server-files.zip' 'https://edge.forgecdn.net/files/4879/990/Vault-Hunters-3rd-Edition-3.12.4-server-files.zip' && unzip -u -o 'Vault-Hunters-3rd-Edition-3.12.4-server-files.zip' -d /data
   curl -Lo forge-${FORGE_VERSION}-installer.jar 'https://maven.minecraftforge.net/net/minecraftforge/forge/'${FORGE_VERSION}'/forge-'${FORGE_VERSION}'-installer.jar'
   java -jar forge-${FORGE_VERSION}-installer.jar --installServer && rm -f forge-${FORGE_VERSION}-installer.jar
 fi
