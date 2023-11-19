a3bea45d48a536b66d683b107a2622e1bc5c7f4d
Jason McLeod <jason@mcleods.me>
2023-11-19 08:13:21 -0500

    Update to version Update-9.0.2

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 
 FROM openjdk:17.0.2-jdk-buster
 
-LABEL version="Update-9.0.3"
+LABEL version="Update-9.0.2"
 
 RUN apt-get update && apt-get install -y curl && \
  adduser --uid 99 --gid 100 --home /data --disabled-password minecraft
--- launch.sh
+++ launch.sh
@@ -20,9 +20,9 @@ else
 fi
 
 
-if ! [[ -f "Vault Hunters 3rd Edition-Update-9.0.3_Server-Files.zip" ]]; then
+if ! [[ -f "Vault Hunters 3rd Edition-Update-9.0.2_Server-Files.zip" ]]; then
   rm -fr config mods defaultconfigs scripts forge-*.jar start.sh *Server.zip
-	curl -Lo 'Vault Hunters 3rd Edition-Update-9.0.3_Server-Files.zip' 'https://edge.forgecdn.net/files/4516/817/Vault Hunters 3rd Edition-Update-9.0.3_Server-Files.zip' && unzip -u -o 'Vault Hunters 3rd Edition-Update-9.0.3_Server-Files.zip' -d /data
+	curl -Lo 'Vault Hunters 3rd Edition-Update-9.0.2_Server-Files.zip' 'https://edge.forgecdn.net/files/4516/204/Vault Hunters 3rd Edition-Update-9.0.2_Server-Files.zip' && unzip -u -o 'Vault Hunters 3rd Edition-Update-9.0.2_Server-Files.zip' -d /data
   curl -Lo forge-${FORGE_VERSION}-installer.jar 'https://maven.minecraftforge.net/net/minecraftforge/forge/'${FORGE_VERSION}'/forge-'${FORGE_VERSION}'-installer.jar'
   java -jar forge-${FORGE_VERSION}-installer.jar --installServer && rm -f forge-${FORGE_VERSION}-installer.jar
 fi
